46c564b7a0a9d50717b408e6efa47ac35085d582
janus156 <1569361808@qq.com>
2023-04-17 13:31:14 +0800

    newbranch01-c1

--- common1/src/main/java/hello/Main.java
+++ common1/src/main/java/hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("hello ");
+        System.out.println("hello branch 11111111111111");
     }
 
 
