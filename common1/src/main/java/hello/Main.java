f8f97cfccd689f9a545d685500ff7711a78038a6
janus156 <1569361808@qq.com>
2023-04-17 13:53:17 +0800

    master change to 5

--- common1/src/main/java/hello/Main.java
+++ common1/src/main/java/hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("hello branch 444444");
+        System.out.println("master change to 5");
     }
 
 
