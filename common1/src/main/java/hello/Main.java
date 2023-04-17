fe0fb13ea75ff670a2d48d4a9da62b75971cfdf8
janus156 <1569361808@qq.com>
2023-04-17 14:03:27 +0800

    master合并

--- common1/src/main/java/hello/Main.java
+++ common1/src/main/java/hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("master change to 5");
+        System.out.println("merge conflict");
     }
 
 
