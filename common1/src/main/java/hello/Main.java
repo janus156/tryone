0ecf7672ea56a6394046894b7bb3807759721ed0
janus156 <1569361808@qq.com>
2023-04-17 14:24:25 +0800

    conflict 222

--- common1/src/main/java/hello/Main.java
+++ common1/src/main/java/hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("merge conflict");
+        System.out.println("merge conflict 222222222222");
     }
 
 
