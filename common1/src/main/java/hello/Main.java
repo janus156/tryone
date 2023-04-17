7e45708dac8680ad294b80ab48d4986978b7b885
janus156 <1569361808@qq.com>
2023-04-17 14:02:34 +0800

    branch01合并

--- common1/src/main/java/hello/Main.java
+++ common1/src/main/java/hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("01 change to 6");
+        System.out.println("merge conflict");
     }
 
 
